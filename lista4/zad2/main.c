e53be908abca13b02ef3ecc438dcdaa9bfd46a5e
jealousprecision <maksymilian.kadukowski@gmail.com>
2020-11-12 17:11:10 +0100

    .2

--- lista4/zad2/main.c
+++ lista4/zad2/main.c
@@ -5,7 +5,6 @@
 
 #define NANO_TO_MILLI 1000000
 
-void signal_handle(int sig);
 void signal_alarm(int sig);
 void signal_term(int sig);
 void signal_usr1(int sig);
@@ -28,23 +27,6 @@ int main()
 	{
 		++i;
 		nanosleep(&tspec, NULL);
-	}
-}
-
-void signal_handle(int sig)
-{
-	switch (sig)
-	{
-	case SIGALRM:
-		printf("Catched SIGALRM\n");
-		break;
-	case SIGTERM:
-		printf("Catched SIGTERM\n");
-		running = false;
-		break;
-	case SIGUSR1:
-		printf("Catched SIGUSR1\n");
-		break;
 	}
 }
 
